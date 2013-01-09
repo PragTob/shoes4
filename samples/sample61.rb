0df9e112cb2876d619d2f798a9825ab01c014174
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-01-09 20:29:28 +0100

    Test snippets from ash for testing of app and width problems

---
+++ samples/sample61.rb
@@ -0,0 +1,4 @@
+Shoes.app width: 300, height: 300 do
+  line 0, 100, 600, 100
+  rect 0, 150, 600, 50
+end
---
+++ samples/sample62.rb
@@ -0,0 +1,4 @@
+# temporary for app height issues
+Shoes.app width: 300, height: 300 do
+  30.times{para 'hello hello'}
+end
